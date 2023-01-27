af54c47a28ebcdc4c29f485b193d401ab2df30df
PearUK <muicamau007@gmail.com>
2023-01-27 12:52:41 +0700

    fix js

--- assets/js/auto-slide-tab.js
+++ assets/js/auto-slide-tab.js
@@ -1,141 +1,141 @@
 var duration, duration2, loop, loop2, loop3, loop4;
 
 $(".div-main-section-opportunities__tabs-nav-item:first-child").hasClass(
-  "div-tab__item_active"
-)
-  ? (duration = 16e3)
-  : !$(".div-main-section-opportunities__tabs-nav-item:first-child").hasClass(
-      "div-tab__item_active"
-    ) || (duration = 9e3),
-  $(".div-main-section-features__tabs-nav-item:first-child").hasClass(
-    "div-tab__item_active"
-  )
-    ? (duration2 = 26e3)
-    : !$(".div-main-section-features__tabs-nav-item:first-child").hasClass(
         "div-tab__item_active"
-      ) || (duration2 = 11e3);
-var loop = window.setInterval(function () {
-    showNextSlide();
-  }, duration),
-  loop2 = window.setInterval(function () {
-    showNextSlide2();
-  }, duration2),
-  loop3 = window.setInterval(function () {
-    showNextSlide3();
-  }, 8e3),
-  loop4 = window.setInterval(function () {
-    showNextSlide4();
-  }, 6e3);
+    ) ?
+    (duration = 16e3) :
+    !$(".div-main-section-opportunities__tabs-nav-item:first-child").hasClass(
+        "div-tab__item_active"
+    ) || (duration = 9e3),
+    $(".div-main-section-features__tabs-nav-item:first-child").hasClass(
+        "div-tab__item_active"
+    ) ?
+    (duration2 = 26e3) :
+    !$(".div-main-section-features__tabs-nav-item:first-child").hasClass(
+        "div-tab__item_active"
+    ) || (duration2 = 11e3);
+var loop = window.setInterval(function() {
+        showNextSlide();
+    }, duration),
+    loop2 = window.setInterval(function() {
+        showNextSlide2();
+    }, duration2),
+    loop3 = window.setInterval(function() {
+        showNextSlide3();
+    }, 8e3),
+    loop4 = window.setInterval(function() {
+        showNextSlide4();
+    }, 6e3);
 
 function showNextSlide() {
-  var t = $(".div-main-section-opportunities .div-tab__panel_active"),
-    e = t.next(),
-    i = $(".div-main-section-opportunities .div-tab__item_active"),
-    a = i.next();
-  0 === e.length &&
-    ($(".div-main-section-opportunities .div-tab__panel:first-child").toggleClass(
-      "div-tab__panel_active"
-    ),
-    $(".div-main-section-opportunities .div-tab__item:first-child").toggleClass(
-      "div-tab__item_active"
-    )),
-    t.removeClass("div-tab__panel_active"),
-    e.toggleClass("div-tab__panel_active"),
-    i.removeClass("div-tab__item_active"),
-    a.toggleClass("div-tab__item_active");
+    var t = $(".div-main-section-opportunities .div-tab__panel_active"),
+        e = t.next(),
+        i = $(".div-main-section-opportunities .div-tab__item_active"),
+        a = i.next();
+    0 === e.length &&
+        ($(".div-main-section-opportunities .div-tab__panel:first-child").toggleClass(
+                "div-tab__panel_active"
+            ),
+            $(".div-main-section-opportunities .div-tab__item:first-child").toggleClass(
+                "div-tab__item_active"
+            )),
+        t.removeClass("div-tab__panel_active"),
+        e.toggleClass("div-tab__panel_active"),
+        i.removeClass("div-tab__item_active"),
+        a.toggleClass("div-tab__item_active");
 }
 
 function showNextSlide2() {
-  var t = $(".div-main-section-features .div-tab__panel_active"),
-    e = t.next(),
-    i = $(".div-main-section-features .div-tab__item_active"),
-    a = i.next();
-  0 === e.length &&
-    ($(".div-main-section-features .div-tab__panel:first-child").toggleClass(
-      "div-tab__panel_active"
-    ),
-    $(".div-main-section-features .div-tab__item:first-child").toggleClass(
-      "div-tab__item_active"
-    )),
-    t.removeClass("div-tab__panel_active"),
-    e.toggleClass("div-tab__panel_active"),
-    i.removeClass("div-tab__item_active"),
-    a.toggleClass("div-tab__item_active");
+    var t = $(".div-main-section-features .div-tab__panel_active"),
+        e = t.next(),
+        i = $(".div-main-section-features .div-tab__item_active"),
+        a = i.next();
+    0 === e.length &&
+        ($(".div-main-section-features .div-tab__panel:first-child").toggleClass(
+                "div-tab__panel_active"
+            ),
+            $(".div-main-section-features .div-tab__item:first-child").toggleClass(
+                "div-tab__item_active"
+            )),
+        t.removeClass("div-tab__panel_active"),
+        e.toggleClass("div-tab__panel_active"),
+        i.removeClass("div-tab__item_active"),
+        a.toggleClass("div-tab__item_active");
 }
 
 function showNextSlide3() {
-  var t = $(
-      ".div-main-section-platforms__tabs-content-item-inner .div-tab__panel_active"
-    ),
-    e = t.next(),
-    i = $(".div-main-section-platforms .div-tab__item_active"),
-    a = i.next();
-  0 === e.length &&
-    ($(
-      ".div-main-section-platforms__tabs-content-item-inner .div-tab__panel:first-child"
-    ).toggleClass("div-tab__panel_active"),
-    $(".div-main-section-platforms .div-tab__item:first-child").toggleClass(
-      "div-tab__item_active"
-    )),
-    t.removeClass("div-tab__panel_active"),
-    e.toggleClass("div-tab__panel_active"),
-    i.removeClass("div-tab__item_active"),
-    a.toggleClass("div-tab__item_active");
+    var t = $(
+            ".div-main-section-platforms__tabs-content-item-inner .div-tab__panel_active"
+        ),
+        e = t.next(),
+        i = $(".div-main-section-platforms .div-tab__item_active"),
+        a = i.next();
+    0 === e.length &&
+        ($(
+                ".div-main-section-platforms__tabs-content-item-inner .div-tab__panel:first-child"
+            ).toggleClass("div-tab__panel_active"),
+            $(".div-main-section-platforms .div-tab__item:first-child").toggleClass(
+                "div-tab__item_active"
+            )),
+        t.removeClass("div-tab__panel_active"),
+        e.toggleClass("div-tab__panel_active"),
+        i.removeClass("div-tab__item_active"),
+        a.toggleClass("div-tab__item_active");
 }
 
 function showNextSlide4() {
-  var t = $(".div-main-section-teams .div-tab__panel_active"),
-    e = t.next(),
-    i = $(".div-main-section-teams .div-tab__item_active"),
-    a = i.next();
-  0 === e.length &&
-    ($(".div-main-section-teams .div-tab__panel:first-child").toggleClass(
-      "div-tab__panel_active"
-    ),
-    $(".div-main-section-teams .div-tab__item:first-child").toggleClass(
-      "div-tab__item_active"
-    )),
-    t.removeClass("div-tab__panel_active"),
-    e.toggleClass("div-tab__panel_active"),
-    i.removeClass("div-tab__item_active"),
-    a.toggleClass("div-tab__item_active");
+    var t = $(".div-main-section-teams .div-tab__panel_active"),
+        e = t.next(),
+        i = $(".div-main-section-teams .div-tab__item_active"),
+        a = i.next();
+    0 === e.length &&
+        ($(".div-main-section-teams .div-tab__panel:first-child").toggleClass(
+                "div-tab__panel_active"
+            ),
+            $(".div-main-section-teams .div-tab__item:first-child").toggleClass(
+                "div-tab__item_active"
+            )),
+        t.removeClass("div-tab__panel_active"),
+        e.toggleClass("div-tab__panel_active"),
+        i.removeClass("div-tab__item_active"),
+        a.toggleClass("div-tab__item_active");
 }
-($.fn.isInViewport = function () {
-  var t = $(this).offset().top,
-    e = t + $(this).outerHeight(),
-    i = $(window).scrollTop(),
-    a = i + $(window).height();
-  return e > i && t < a;
+($.fn.isInViewport = function() {
+    var t = $(this).offset().top,
+        e = t + $(this).outerHeight(),
+        i = $(window).scrollTop(),
+        a = i + $(window).height();
+    return e > i && t < a;
 }),
-  $(window).on("scroll.once", function () {
+$(window).on("scroll.once", function() {
+        $(".div-main-section-opportunities").isInViewport() &&
+            $(window).off("scroll.once");
+    }),
+    $(window).on("scroll.two", function() {
+        $(".div-main-section-features").isInViewport() &&
+            $(window).off("scroll.two");
+    }),
+    $(window).on("scroll.three", function() {
+        $(".div-main-section-platforms").isInViewport() &&
+            $(window).off("scroll.three");
+    }),
+    $(window).on("scroll.four", function() {
+        $(".div-main-section-teams").isInViewport() && $(window).off("scroll.four");
+    }),
     $(".div-main-section-opportunities").isInViewport() &&
-      $(window).off("scroll.once");
-  }),
-  $(window).on("scroll.two", function () {
-    $(".div-main-section-features").isInViewport() &&
-      $(window).off("scroll.two");
-  }),
-  $(window).on("scroll.three", function () {
-    $(".div-main-section-platforms").isInViewport() &&
-      $(window).off("scroll.three");
-  }),
-  $(window).on("scroll.four", function () {
-    $(".div-main-section-teams").isInViewport() && $(window).off("scroll.four");
-  }),
-  $(".div-main-section-opportunities").isInViewport() &&
     $(window).off("scroll.once"),
-  $(".div-main-section-features").isInViewport() && $(window).off("scroll.two"),
-  $(".div-main-section-platforms").isInViewport() &&
+    $(".div-main-section-features").isInViewport() && $(window).off("scroll.two"),
+    $(".div-main-section-platforms").isInViewport() &&
     $(window).off("scroll.three"),
-  $(".div-main-section-teams").isInViewport() && $(window).off("scroll.four");
+    $(".div-main-section-teams").isInViewport() && $(window).off("scroll.four");
 
 var mutinyVideo = document.getElementById("mutiny-video");
 
 const playPromise = mutinyVideo.play() || Promise.reject(""); // Not all browsers return promise from .play().
 playPromise
-  .then(() => {})
-  .catch((err) => {
-    // Video couldn't be autoplayed because of autoplay policy. Mute it and play.
-    mutinyVideo.muted = true;
-    mutinyVideo.play();
-  });
+    .then(() => {})
+    .catch((err) => {
+        // Video couldn't be autoplayed because of autoplay policy. Mute it and play.
+        mutinyVideo.muted = true;
+        mutinyVideo.play();
+    });
--- assets/js/click-tab.js
+++ assets/js/click-tab.js
@@ -9,14 +9,14 @@ $(".div-main-section-reviews__carousel").slick({
     adaptiveHeight: true,
     fade: true,
     prevArrow: $(
-    ".div-main-section-reviews__carousel-slider-btn-wrap .prev-slide"
+        ".div-main-section-reviews__carousel-slider-btn-wrap .prev-slide"
     ),
     nextArrow: $(
-    ".div-main-section-reviews__carousel-slider-btn-wrap .next-slide"
+        ".div-main-section-reviews__carousel-slider-btn-wrap .next-slide"
     ),
 });
 
-$("[div-data-tab-nav]").on("click", function (e) {
+$("[div-data-tab-nav]").on("click", function(e) {
     e.preventDefault();
     e.stopPropagation();
 
@@ -24,36 +24,36 @@ $("[div-data-tab-nav]").on("click", function (e) {
     var target = $this.attr("div-data-tab-nav");
 
     if (
-    $this.closest(".div-main-section-opportunities__tabs-nav").length
+        $this.closest(".div-main-section-opportunities__tabs-nav").length
     ) {
-    window.clearInterval(loop);
-    loop = null;
+        window.clearInterval(loop);
+        loop = null;
     } else if (
-    $this.closest(".div-main-section-features__tabs-nav").length
+        $this.closest(".div-main-section-features__tabs-nav").length
     ) {
-    window.clearInterval(loop2);
-    loop2 = null;
+        window.clearInterval(loop2);
+        loop2 = null;
     }
     if ($this.closest(".div-main-section-platforms__tabs-nav").length) {
-    window.clearInterval(loop3);
-    loop3 = null;
+        window.clearInterval(loop3);
+        loop3 = null;
     } else if ($this.closest(".div-main-section-teams__tabs-nav").length) {
-    window.clearInterval(loop4);
-    loop4 = null;
-    /*  $('.div-main-section-teams__tabs-nav.show-on-desktop .div-main-section-teams__tabs-nav-item-item').each(function(index) {
-    if($(this).hasClass('div-tab__item_active'))
-        $('.hide-on-desktop .div-main-section-teams__tabs-nav-item:nth-child('+(index+1)+')').toggleClass('div-tab__item_active');
-    });  */
+        window.clearInterval(loop4);
+        loop4 = null;
+        /*  $('.div-main-section-teams__tabs-nav.show-on-desktop .div-main-section-teams__tabs-nav-item-item').each(function(index) {
+        if($(this).hasClass('div-tab__item_active'))
+            $('.hide-on-desktop .div-main-section-teams__tabs-nav-item:nth-child('+(index+1)+')').toggleClass('div-tab__item_active');
+        });  */
     }
     $this
-    .addClass("div-tab__item_active")
-    .siblings()
-    .removeClass("div-tab__item_active");
+        .addClass("div-tab__item_active")
+        .siblings()
+        .removeClass("div-tab__item_active");
 
     $("[div-data-tab-panel='" + target + "']")
-    .addClass("div-tab__panel_active")
-    .siblings()
-    .removeClass("div-tab__panel_active");
+        .addClass("div-tab__panel_active")
+        .siblings()
+        .removeClass("div-tab__panel_active");
 
     return false;
 });
